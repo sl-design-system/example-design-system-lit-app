cfe5660b30cc9757f891e958e6efae64a7733533
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2023-03-10 10:08:43 +0100

    Basic setup.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,26 @@
-import { html, LitElement, TemplateResult } from 'lit';
+import type { ScopedElementsMap } from '@open-wc/scoped-elements';
+import type { CSSResultGroup, TemplateResult } from 'lit';
+import { ScopedElementsMixin } from '@open-wc/scoped-elements';
+import { Button } from '@sanomalearning/slds-core/button';
+import { css, html, LitElement } from 'lit';
 
-export class App extends LitElement {
+export class App extends ScopedElementsMixin(LitElement) {
+  static get scopedElements(): ScopedElementsMap {
+    return {
+      'sl-button': Button
+    };
+  }
+
+  static styles: CSSResultGroup = css`
+    :host { display: block; }
+  `;
+
   render(): TemplateResult {
-    return html`HELLO WORLD!`;
+    return html`
+      <h1>Sanoma Learning Design System Demo App</h1>
+      <h2>Using <a href="https://lit.dev">Lit</a></h2>
+      <sl-button>Button</sl-button>
+    `;
   }
 }
 
--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,10 +1,9 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 
 export default {
+  appIndex: './index.html',
   nodeResolve: {
     exportConditions: ['development']    
   },
-  appIndex: './src/index.html',
-  rootDir: 'src',
   plugins: [esbuildPlugin({ ts: true, target: 'auto' })]
 };
