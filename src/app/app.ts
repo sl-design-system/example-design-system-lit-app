232a2fd3c0cdbe51dbe6c0a69c6099ac5cd23bda
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2023-03-23 08:54:46 +0100

    🚤

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,20 @@ import type { ScopedElementsMap } from '@open-wc/scoped-elements';
 import type { CSSResultGroup, TemplateResult } from 'lit';
 import { ScopedElementsMixin } from '@open-wc/scoped-elements';
 import { Button } from '@sanomalearning/slds-core/button';
+import { ButtonBar } from '@sanomalearning/slds-core/button-bar';
+import { Editor } from '@sanomalearning/slds-editor';
+import { Grid, GridColumn } from '@sanomalearning/slds-grid';
 import { css, html, LitElement } from 'lit';
+import { people } from '../data.js';
 
 export class App extends ScopedElementsMixin(LitElement) {
   static get scopedElements(): ScopedElementsMap {
     return {
-      'sl-button': Button
+      'sl-button': Button,
+      'sl-button-bar': ButtonBar,
+      'sl-editor': Editor,
+      'sl-grid': Grid,
+      'sl-grid-column': GridColumn
     };
   }
 
@@ -15,13 +23,60 @@ export class App extends ScopedElementsMixin(LitElement) {
     :host {
       display: block;
     }
+
+    .container {
+      display: flex;
+      gap: 0.5rem;
+      flex-direction: column;
+      flex-wrap: wrap;
+    }
+
+    sl-grid {
+      height: 300px;
+      overflow: auto;
+    }
+  `;
+
+  richText = `
+    <h1>Rich Text Editor</h1>
+    <p>This component is a <em>rich text editor</em> based on the <a href="https://prosemirror.net/" target="_blank">ProseMirror</a> library.</p>
+    <p>It has support for the following editor actions:</p>
+    <ul>
+      <li><p>Typography: <strong>bold</strong>, <em>italic</em>, <u>underline</u> and <del>strikethrough</del></p></li>
+      <li><p>Format: paragraph, quotation and headings</p></li>
+      <li><p>Alignment: left, right or center</p></li>
+      <li><p>Lists: ordered or unordered</p></li>
+      <li><p>Indentation: indent and outdent</p></li>
+      <li><p>Insert links</p></li>
+    </ul>
   `;
 
   render(): TemplateResult {
     return html`
       <h1>Sanoma Learning Design System Demo App</h1>
       <h2>Using <a href="https://lit.dev">Lit</a></h2>
-      <sl-button>Button</sl-button>
+
+      <div class="container">
+        <sl-button-bar>
+          <sl-button>Button</sl-button>
+          <sl-button variant="default">Button</sl-button>
+          <sl-button variant="primary">Button</sl-button>
+          <sl-button variant="success">Button</sl-button>
+          <sl-button variant="warning">Button</sl-button>
+          <sl-button variant="danger">Button</sl-button>
+          <sl-button disabled>Button</sl-button>          
+        </sl-button-bar>
+
+        <sl-editor .value=${this.richText}></sl-editor>
+
+        <sl-grid .items=${people}>
+          <sl-grid-column path="firstName"></sl-grid-column>
+          <sl-grid-column path="lastName"></sl-grid-column>
+          <sl-grid-column path="email"></sl-grid-column>
+          <sl-grid-column path="address.phone"></sl-grid-column>
+          <sl-grid-column path="profession"></sl-grid-column>
+        </sl-grid>
+      </div>
     `;
   }
 }
